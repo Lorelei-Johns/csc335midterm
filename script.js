b1fd7e8fb07f62987a200f991b7c119b10f6721d
ldjohns001 <ldjohns001@converse.edu>
2018-10-18 15:56:53 -0400

    first attempt

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-$(function(){
-     // Add your javascript here
+$(function changeBackgroundColor(){
+    $('#buttondiv input').on('click', function(){
+        $(this).css('background-color', $(this).val());
+        
+    });
+    
+    
+  $('listdiv input').on('click', function(){
+      $.getJSON('GSP-Fire-Stations.json', function(data){
+          $.each(data, function(item){
+              
+          })
+      })
+  })  
+  function makeList() {
+            const urlStr = insertJSON('GSP-Fire-Stations.json');
 
-         
+}  
+    
 });
+
